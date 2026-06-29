148a0464b2588d02642d466c00711d98b05e5664
WSVPN-OKAK <heelloowworld603@gmail.com>
2026-06-29 14:53:31 +0300

    Update wsvpn.py

--- wsvpn.py
+++ wsvpn.py
@@ -21,5 +21,5 @@ def main():
     print("Бот запущен...")
     app_bot.run_polling()
 
-if name == "main":  # ← ВАЖНО! ДВА ПОДЧЕРКИВАНИЯ!
+if _name_ == "_main_":  # ← ВАЖНО! ДВА ПОДЧЕРКИВАНИЯ!
     main()
